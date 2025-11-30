b56340df5ac2694eafd2b2d6b85b9790bf9b9102
MarcusKoen <37168149+MarcusKoen@users.noreply.github.com>
2025-11-30 23:26:00 +0200

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,29 @@
-test
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+
+    int sum = 0;
+
+
+    int num = 1000;
+
+    for(int i=1;i<num;i++)
+    {
+        if(i%3==0)
+        {
+            sum += i;
+        }
+        else if(i%5==0)
+        {
+            sum +=i;
+        }
+    }
+
+    qDebug() << sum;
+
+    return a.exec();
+}
